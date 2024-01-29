8eb4861ae562b5ec2f32d0ca824a0ed358af3b89
Getnet Alemye <debreroha12@gmail.com>
2024-01-29 16:32:20 +0300

    collected file of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
-function App() {
+function App() {  
   return (
-    <div>
-      <h1>welcome</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+
+    </>
   );
 }
 
