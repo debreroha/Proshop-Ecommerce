780838fa1bf2f861d2200fe454319c43a133b062
Getnet Alemye <debreroha12@gmail.com>
2024-02-03 19:57:44 +0300

    fetching the products from the databasse using products model

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from "./config/db.js";
-import products from './data/products.js'
+// import products from './data/products.js'
 import Product from "./models/productModel.js";
 const port = process.env.PORT || 5000
 
@@ -15,19 +15,30 @@ app.get('/', (req, res) => {
     res.send('API is running')
 })
 
-app.get('/api/products', (req, res) => {3
-    res.json(products)
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
 })
 
-app.get('/api/products/:id', (req, res) =>{
+app.get('/api/products/:id',  async (req, res) =>{
     
-    const product = products.find((p) => p._id == req.params.id)
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product not found' });
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
     }
-    
 })
 
 app.listen(port, () => console.log(`server running on port ${port}`)) 
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -38,6 +38,7 @@ const ProductScreen = () => {
             <ListGroup variant='flush'>
                 <ListGroup.Item>
                     <h3>{product.name}</h3>
+                    {/* <h3>{product._id}</h3> */}
                 </ListGroup.Item>
                 <ListGroup.Item>
                     <Rating value={product.rating} text ={`${product.numReviews} reviews`} />
