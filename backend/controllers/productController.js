c633467f004d0d049d9a4a7d3b52c88653a23f9c
Getnet Alemye <debreroha12@gmail.com>
2024-02-04 15:26:41 +0300

    finishing database part

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import asyncHandler from '../middleware/asyncHandler.js'
+import Product from '../models/productModel.js'
+
+//fetch all products
+// GET /api/products
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+        res.json(products)
+})
+
+
+// fetch a product
+// GET /api/products/:id
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(product){
+        return res.json(product)
+    }else{
+        res.status(404)
+        throw new("Resource not found")
+    }
+})
+
+export {getProducts, getProductById}
