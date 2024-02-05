f2718d61041b0f4ee50820bb826ff389b35e0ae0
Getnet Alemye <debreroha12@gmail.com>
2024-02-05 23:54:02 +0300

    implementing redux toolkit and react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider } from 'react-router-dom';
+  import { Provider } from 'react-redux';
+  import store from './store';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './assets/styles/bootstrap.custom.css';
@@ -10,29 +17,33 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path='/' element={<App />}>
-//       <Route index={true} path='/' element={<HomeScreen />} ></Route>
-//       <Route path='/product/:id' element={<ProductScreen />} ></Route>
-//     </Route>
-//   )
-// )
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} ></Route>
+      <Route path='/product/:id' element={<ProductScreen />} ></Route>
+    </Route>
+  )
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index={true} path="/" element={<HomeScreen />} />
-          <Route path="/product/:id" element={<ProductScreen />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
+
+// Router>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route index={true} path="/" element={<HomeScreen />} />
+//           <Route path="/product/:id" element={<ProductScreen />} />
+//         </Route>
+//       </Routes>
+//     </Router>
  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
