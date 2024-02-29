d198745041c35d3662cc1f99730460a64e682bd7
Getnet Alemye <debreroha12@gmail.com>
2024-02-29 13:28:32 +0300

    backend authetication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,11 +11,12 @@ import {
     getUsers,
     deleteUser,
 } from '../controllers/userController.js'
+import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(getUsers)
 router.post('/logout', logoutUser)
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/:id').delete(deleteUser).get(getUserByID).put(updateUser)
 
 export default router
