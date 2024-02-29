24e07a82059a7abbd4622ea38c74eb6dd9c5a573
Getnet Alemye <debreroha12@gmail.com>
2024-02-29 18:42:18 +0300

    loginScreen

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config() 
+import path from 'path';
+
+export default {
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify')
+    }
+  }
+};
+
+
